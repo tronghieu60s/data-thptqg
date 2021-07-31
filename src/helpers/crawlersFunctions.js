2b4b3aaff6e956574e6442776fd131be4b142a69
Trong Hieu <tronghieu60s@gmail.com>
2021-07-31 17:15:41 +0700

    select options before crawlers

--- src/helpers/crawlersFunctions.js
+++ src/helpers/crawlersFunctions.js
@@ -1,7 +1,25 @@
+const {
+  writeObjectDataToJson,
+  writeObjectDataToCsv,
+} = require("./commonFunctions");
+
 function getIdfNumber(frontNumber, rearNumber) {
   const frontIdfNum = ("00" + frontNumber).slice(-2);
   const rearIdfNum = ("000000" + rearNumber).slice(-6);
   return `${frontIdfNum}${rearIdfNum}`;
 }
 
-module.exports = { getIdfNumber };
+function writeDataToFile(typeExport, resultData) {
+  switch (typeExport) {
+    case "file json":
+      writeObjectDataToJson("./data/DiemThi2021.json", resultData);
+      break;
+    case "file csv":
+      writeObjectDataToCsv("./data/DiemThi2021.csv", resultData);
+      break;
+    default:
+      break;
+  }
+}
+
+module.exports = { getIdfNumber, writeDataToFile };
---
+++ src/helpers/questionsToStart.js
@@ -0,0 +1,30 @@
+const inquirer = require("inquirer");
+
+const mainQuestions = [
+  {
+    type: "confirm",
+    name: "headless",
+    message: "Bạn có muốn chạy crawler trong chế độ ẩn không?",
+    default: true,
+  },
+  {
+    type: "list",
+    name: "typeExport",
+    message: "Vui lòng chọn kiểu dữ liệu cần xuất.",
+    choices: ["File JSON", "File CSV"],
+    filter(val) {
+      return val.toLowerCase();
+    },
+  },
+];
+
+module.exports = function questionsToStart() {
+  return inquirer
+    .prompt(mainQuestions)
+    .then((answers) => {
+      // Use user feedback for... whatever!!
+      console.log("Cài đặt hoàn tất, crawlers sẽ bắt đầu sau vài giây nữa...");
+      return answers;
+    })
+    .catch((err) => console.error(err));
+};
