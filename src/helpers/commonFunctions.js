d971a17e273be51efc11d16bd3ec0e48eb3cbbca
Trong Hieu <tronghieu60s@gmail.com>
2021-08-01 09:05:37 +0700

    increase speed crawlers

--- src/helpers/commonFunctions.js
+++ src/helpers/commonFunctions.js
@@ -10,23 +10,34 @@ function randomIntFromInterval(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function writeObjectDataToJson(filePath, objectData) {
+function writeDataToJson(filePath, data) {
   let fileData = [];
   try {
     fileData = JSON.parse(fs.readFileSync(filePath));
   } catch (err) {}
-  fileData.push(objectData);
+
+  if (Array.isArray(data)) {
+    fileData.push(...data);
+  } else if (typeof data === "object") {
+    fileData.push(data);
+  }
+
   fs.writeFileSync(filePath, JSON.stringify(fileData, null, 2));
 }
 
-function writeObjectDataToCsv(filePath, objectData) {
-  const csv = new ObjectsToCsv([objectData]);
+function writeDataToCsv(filePath, data) {
+  let csv = new ObjectsToCsv([]);
+  if (Array.isArray(data)) {
+    csv = new ObjectsToCsv([...data]);
+  } else if (typeof data === "object") {
+    csv = new ObjectsToCsv([data]);
+  }
   return csv.toDisk(filePath, { append: true });
 }
 
 module.exports = {
   isObjectEmpty,
   randomIntFromInterval,
-  writeObjectDataToJson,
-  writeObjectDataToCsv,
+  writeDataToJson,
+  writeDataToCsv,
 };
--- src/helpers/crawlersFunctions.js
+++ src/helpers/crawlersFunctions.js
@@ -1,21 +1,20 @@
-const {
-  writeObjectDataToJson,
-  writeObjectDataToCsv,
-} = require("./commonFunctions");
+const { writeDataToJson, writeDataToCsv } = require("./commonFunctions");
 
+/* identification number consists of 8 digits
+here only create the first 6 digits, the last 2 digits will run from 00 - 99 */
 function getIdfNumber(frontNumber, rearNumber) {
   const frontIdfNum = ("00" + frontNumber).slice(-2);
-  const rearIdfNum = ("000000" + rearNumber).slice(-6);
+  const rearIdfNum = ("0000" + rearNumber).slice(-4);
   return `${frontIdfNum}${rearIdfNum}`;
 }
 
 function writeDataToFile(typeExport, resultData) {
   switch (typeExport) {
     case "file json":
-      writeObjectDataToJson("./data/DiemThi2021.json", resultData);
+      writeDataToJson("./data/DiemThi2021.json", resultData);
       break;
     case "file csv":
-      writeObjectDataToCsv("./data/DiemThi2021.csv", resultData);
+      writeDataToCsv("./data/DiemThi2021.csv", resultData);
       break;
     default:
       break;
