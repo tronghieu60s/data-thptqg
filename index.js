f55a44025cf0c790c4bdc5fffbd42f50d16dc298
Trong Hieu <tronghieu60s@gmail.com>
2021-08-01 16:32:35 +0700

    update thanhnien.vn

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const fs = require("fs");
 const puppeteer = require("puppeteer");
 const tienPhong = require("./src/crawlers/tienphong.vn");
+const thanhNien = require("./src/crawlers/thanhnien.vn");
 const questionsToStart = require("./src/helpers/questionsToStart");
+
+function isBrowserHeadless(type_crawler) {
+  let headless = true;
+  if (process.env.NODE_ENV.trim() === "development") {
+    headless = false;
+  } else if (type_crawler.indexOf("perform") > -1) {
+    headless = false;
+  }
+  return headless;
+}
 
 (async () => {
   const options = await questionsToStart();
@@ -13,15 +24,17 @@ const questionsToStart = require("./src/helpers/questionsToStart");
     fs.mkdirSync(dirName);
   }
 
-  const headless = type_crawler.indexOf("perform") === -1;
   const browser = await puppeteer.launch({
-    headless,
+    headless: isBrowserHeadless(type_crawler),
     args: ["--start-maximized"],
     // devtools: true,
   });
 
+  /* crawlers data */
   if (type_crawler.indexOf("tienphong") > -1) {
     await tienPhong(browser, options);
+  } else if (type_crawler.indexOf("thanhnien") > -1) {
+    await thanhNien(browser, options);
   }
 
   await browser.close();
